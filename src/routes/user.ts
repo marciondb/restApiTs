e68577cc516728d80c8a08e55b0f3e01430615ad
Márcio Dias <marciondb@gmail.com>
2019-08-18 18:24:51 -0300

    fix rota

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,17 +5,17 @@ import { checkJwt } from '../middlewares/checkJwt'
 
 const router = Router()
 
-router.get('/user', checkJwt, UserController.listAll)
+router.get('/', UserController.listAll)
 
-router.get('/user:id([0-9]+)', checkJwt, UserController.getOneById
+router.get('/:id([0-9]+)', checkJwt, UserController.getOneById
 )
 
-router.post('/user', checkJwt, UserController.newUser)
+router.post('/', checkJwt, UserController.newUser)
 
-router.patch('/user:id([0-9]+)', checkJwt, UserController.editUser
+router.patch('/:id([0-9]+)', checkJwt, UserController.editUser
 )
 
-router.delete('/user:id([0-9]+)', checkJwt, UserController.deleteUser
+router.delete('/:id([0-9]+)', checkJwt, UserController.deleteUser
 )
 
 export default router
