da592e7fe5d8f59d44413ef0cebd220c981d7baa
Márcio Dias <marciondb@gmail.com>
2019-08-18 17:38:42 -0300

    fix para dar start

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,24 @@
-import app from './app'
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import * as helmet from 'helmet'
+import * as cors from 'cors'
+import routes from './routes'
 
-app.listen(3333)
+createConnection()
+  .then(async () => {
+    const app = express()
+
+    // Call midlewares
+    app.use(cors())
+    app.use(helmet())
+    app.use(bodyParser.json())
+
+    app.use('/', routes)
+
+    app.listen(3333, () => {
+      console.log('Server started on port 3333!')
+    })
+  })
+  .catch(error => console.log(error))
