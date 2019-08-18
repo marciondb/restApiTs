392f0fa84e958c9a597c1b9fbe20b8890f910685
Márcio Dias <marciondb@gmail.com>
2019-08-18 18:09:01 -0300

    renomean do rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,4 +7,8 @@ const routes = Router()
 routes.use('/auth', auth)
 routes.use('/user', user)
 
+routes.get('/', (req, res) => {
+  return res.send('Hello Orbita')
+})
+
 export default routes
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,17 +5,17 @@ import { checkJwt } from '../middlewares/checkJwt'
 
 const router = Router()
 
-router.get('/', checkJwt, UserController.listAll)
+router.get('/user', checkJwt, UserController.listAll)
 
-router.get('/:id([0-9]+)', checkJwt, UserController.getOneById
+router.get('/user:id([0-9]+)', checkJwt, UserController.getOneById
 )
 
-router.post('/', checkJwt, UserController.newUser)
+router.post('/user', checkJwt, UserController.newUser)
 
-router.patch('/:id([0-9]+)', checkJwt, UserController.editUser
+router.patch('/user:id([0-9]+)', checkJwt, UserController.editUser
 )
 
-router.delete('/:id([0-9]+)', checkJwt, UserController.deleteUser
+router.delete('/user:id([0-9]+)', checkJwt, UserController.deleteUser
 )
 
 export default router
