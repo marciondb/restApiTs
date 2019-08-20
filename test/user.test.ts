72c4453c8b46e147c426787e9b4fdbf234655b99
Márcio Dias <marciondb@gmail.com>
2019-08-20 12:16:18 -0300

    test init

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,6 +1,18 @@
 /* eslint-disable no-undef */
 // import { sum } from '../foo'
-
-test('basic', () => {
-  expect(1).toBe(1)
+import * as request from 'supertest'
+// import app from '../src/app'
+const requests = request('http://localhost:3333')
+describe('log in API', function () {
+  it('responds with json', async (done) => {
+    await requests
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'admin@admin.com',
+        password: 'admin'
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+  }, 30000)
 })
