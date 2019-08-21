ea73daae08d99c216be72c04af6b530600c5df54
Márcio Dias <marciondb@gmail.com>
2019-08-21 15:51:44 -0300

    BE dasafio DONE º/

--- src/controllers/PanelInstallationController.ts
+++ src/controllers/PanelInstallationController.ts
@@ -7,13 +7,20 @@ import { PanelInstallation } from '../model/PanelInstallation'
 
 class PanelInstallationController {
   public listByUserStateId = async (req: Request, res: Response): Promise<Response> => {
+    const skip: number = req.params['skip']
+    console.log(skip)
     const userStateId = res.locals.jwtPayload.userStateId
     const panelInstallationRepository = getRepository(PanelInstallation)
 
     const panelInstallations = await panelInstallationRepository.find({
       select: ['id', 'dataProvider', 'system_size', 'zipcode', 'stateId', 'cost', 'installation_date'],
       relations: ['dataProvider', 'stateId'],
-      where: { stateId: userStateId }
+      where: { stateId: userStateId },
+      order: {
+        id: 'ASC'
+      },
+      skip: skip,
+      take: 1000
     })
 
     return res.send(panelInstallations)
--- src/routes/panelInstallation.ts
+++ src/routes/panelInstallation.ts
@@ -6,15 +6,21 @@ const router = Router()
 
 /**
 * @swagger
-* /panelInstallation/listByUserStateId:
+* /panelInstallation/listByUserStateId/{skip}:
 *    get:
 *      tags:
 *        - Panel Installation
 *      summary: List Panel Installation by logged user state id
-*      description: List all Panel Installation by user's state id.
+*      description: List all Panel Installation by user's state id. Is limited to 1000 entities per get
 *      operationId: listByUserStateId
 *      produces:
 *        - application/json
+*      parameters:
+*        - in: path
+*          name: skip
+*          description: offset (paginated) from where entities should be taken.
+*          required: true
+*          type: number
 *      responses:
 *        default:
 *          description: list of Panel Installation
@@ -23,6 +29,6 @@ const router = Router()
 *            items:
 *              $ref: "#/definitions/PanelInstallation"
 */
-router.get('/listByUserStateId', checkJwt, PanelInstallationController.listByUserStateId)
+router.get('/listByUserStateId/:skip([0-9]+)', checkJwt, PanelInstallationController.listByUserStateId)
 
 export default router
