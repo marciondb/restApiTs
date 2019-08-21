c5e15a4119af20576ae77c314668b83b73454b61
Márcio Dias <marciondb@gmail.com>
2019-08-20 22:23:26 -0300

    BE dasafio DONE º/

--- src/controllers/PanelInstallationController.ts
+++ src/controllers/PanelInstallationController.ts
@@ -1,16 +1,19 @@
 /* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable camelcase */
 import { Request, Response } from 'express'
-import { getRepository } from 'typeorm'
+import { getRepository, In } from 'typeorm'
 
 import { PanelInstallation } from '../model/PanelInstallation'
 
 class PanelInstallationController {
   public listByUserStateId = async (req: Request, res: Response): Promise<Response> => {
+    const userStateId = res.locals.jwtPayload.userStateId
     const panelInstallationRepository = getRepository(PanelInstallation)
+
     const panelInstallations = await panelInstallationRepository.find({
       select: ['id', 'dataProvider', 'system_size', 'zipcode', 'stateId', 'cost', 'installation_date'],
-      relations: ['dataProvider', 'stateId']
+      relations: ['dataProvider', 'stateId'],
+      where: { stateId: userStateId }
     })
 
     return res.send(panelInstallations)
